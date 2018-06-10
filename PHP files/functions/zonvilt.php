7ea26d9e1879024b00b13e774b4c5ca3cb006717
shootinggame82 <andreas@radiosandviken.se>
2018-06-10 19:32:36 +0200

    Create zonvilt.php

---
+++ PHP files/functions/zonvilt.php
@@ -0,0 +1,36 @@
+<?php
+include '../db.php';
+$sensor = $_POST['sensor'];
+$zonid = $_POST['zonid'];
+
+$existerar = 1;
+
+
+if (isset($zonid, $sensor)) {
+    $sql = "SELECT * FROM zonvilt WHERE zon = '$zonid' AND vilt = '$sensor'";
+    $result = $mysqli->query($sql);
+
+    if ($result->num_rows == 1) {
+        $existerar = 1;
+        $echodata = array('error' => 'true', 'errorcode' => '0');
+        echo json_encode($echodata);
+    } else {
+        $existerar = 0;
+    }
+
+    if ($existerar == 0) {
+        if ($insert_stmt = $mysqli->prepare("INSERT INTO zonvilt (zon, vilt) VALUES (?, ?)")) {
+            $insert_stmt->bind_param('ss', $zonid, $sensor);
+            if (! $insert_stmt->execute()) {
+                $echodata = array('error' => 'true', 'errorcode' => '1');
+                echo json_encode($echodata);
+                exit();
+            }
+            $echodata = array('error' => 'false', 'errorcode' => '0');
+            echo json_encode($echodata);
+        }
+    }
+} else {
+    $echodata = array('error' => 'true', 'errorcode' => '2');
+    echo json_encode($echodata);
+}
